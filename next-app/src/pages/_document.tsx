43b55168b83794463e34548a031ff3503285c22a
Dinesh Pandiyan <flexdinesh@gmail.com>
2022-09-03 12:05:28 +1000

    Use emoji as favicon

--- next-app/src/pages/_document.tsx
+++ next-app/src/pages/_document.tsx
@@ -6,6 +6,10 @@ export default function Document() {
     <Html>
       <Head>
         <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋</text></svg>"
+        />
+        <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;0,800;1,400;1,700&display=swap"
           rel="stylesheet"
         />
