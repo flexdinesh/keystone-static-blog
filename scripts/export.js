75d7bef6193c06b6612d9c7dbd8be44eda4b2886
Dinesh Pandiyan <flexdinesh@gmail.com>
2022-09-11 19:45:45 +1000

    Merge export scripts

--- scripts/export.js
+++ scripts/export.js
@@ -1,46 +1,62 @@
 const concurrently = require('concurrently');
-const { result } = concurrently(
-  [
-    { command: 'yarn start:keystone', name: 'KEYSTONE', prefixColor: 'green' },
+
+if (process.argv.includes('--docker')) {
+  const { result } = concurrently(
+    [
+      {
+        command: 'yarn keystone:docker:build && yarn keystone:docker:run',
+        name: 'KEYSTONE',
+        prefixColor: 'green',
+      },
+      {
+        command:
+          'wait-on http://localhost:3000 && yarn export:app && yarn keystone:docker:kill && wait-on -r http://localhost:3000',
+        name: 'BUILD-AND-KILL',
+        prefixColor: 'magenta',
+      },
+    ],
     {
-      command:
-        'wait-on http://localhost:3000 && yarn export:app && yarn kill:server',
-      name: 'BUILD-AND-KILL',
-      prefixColor: 'magenta',
+      prefix: 'name',
+      /* 
+        First command exits with a SIGKILL and exit code 1 when killed with kill-port in Mac.
+        So we ignore that using successCondition
+      */
+      successCondition: 'command-1',
+    }
+  );
+  result.then(
+    success => {
+      console.log('✅ Export Success!');
     },
-  ],
-  {
-    prefix: 'name',
-    /* 
-      First command exits with a SIGKILL and exit code 1 when killed with kill-port in Mac.
-      So we ignore that using successCondition
-    */
-    successCondition: 'command-1',
-  }
-);
-result.then(
-  success => {
-    console.log('✅ Export Success!');
-    // success.forEach(c => {
-    //   const {
-    //     index,
-    //     exitCode,
-    //     killed,
-    //     command: { exited, name, command },
-    //   } = c;
-    //   console.log({ index, exitCode, killed, exited, name, command });
-    // });
-  },
-  failure => {
-    console.log('❌ Export Failed!');
-    // failure.forEach(f => {
-    //   const {
-    //     index,
-    //     exitCode,
-    //     killed,
-    //     command: { exited, name, command },
-    //   } = f;
-    //   console.log({ index, exitCode, killed, exited, name, command });
-    // });
-  }
-);
+    failure => {
+      console.log('❌ Export Failed!');
+    }
+  );
+} else {
+  const { result } = concurrently(
+    [
+      { command: 'yarn start:keystone', name: 'KEYSTONE', prefixColor: 'green' },
+      {
+        command: 'wait-on http://localhost:3000 && yarn export:app && yarn kill:keystone',
+        name: 'BUILD-AND-KILL',
+        prefixColor: 'magenta',
+      },
+    ],
+    {
+      prefix: 'name',
+      /* 
+        First command exits with a SIGKILL and exit code 1 when killed with kill-port in Mac.
+        So we ignore that using successCondition
+      */
+      successCondition: 'command-1',
+    }
+  );
+  result.then(
+    success => {
+      console.log('✅ Export Success!');
+    },
+    failure => {
+      console.log('❌ Export Failed!');
+    }
+  );
+}
