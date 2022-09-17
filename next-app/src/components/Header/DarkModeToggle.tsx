a702d9e16f5412785717305c542daf96d95d4c46
Dinesh Pandiyan <flexdinesh@gmail.com>
2022-09-17 20:53:18 +1000

    Fancy dark mode toggle

--- next-app/src/components/Header/DarkModeToggle.tsx
+++ next-app/src/components/Header/DarkModeToggle.tsx
@@ -2,6 +2,27 @@ import React from 'react';
 import { Switch } from '@headlessui/react';
 import { useDarkMode } from './useDarkMode';
 
+function SunMoon({ mode }: { mode: 'dark' | 'light' }) {
+  return (
+    <span
+      className={`
+        inline-block h-4 w-4 transition-all duration-300 rounded-full bg-gradient-to-tr 
+        ${
+          mode === 'dark'
+            ? 'translate-x-6 from-primary-200 to-primary-800'
+            : 'translate-x-1 from-primary-200 to-primary-800'
+        }
+        `}
+    >
+      <span
+        className={`absolute top-0 right-0 w-[11px] h-[11px] rounded-full bg-gray-700
+          ${mode === 'dark' ? 'scale-[1]' : 'scale-[0]'}
+          `}
+      ></span>
+    </span>
+  );
+}
+
 /*
   since we don't know the user's saved dark/light during server rendering 
   we're doing this not-great-but-good-enough trick —
@@ -10,24 +31,16 @@ import { useDarkMode } from './useDarkMode';
 export function DarkModeToggle() {
   const { mode, toggleMode } = useDarkMode();
 
-  const toggleBackgroundClassname =
+  const bgColor =
     mode === null ? 'bg-transparent' : mode === 'dark' ? 'bg-gray-700' : 'bg-gray-200';
   return (
     <Switch
       checked={mode === 'dark'}
       onChange={toggleMode}
-      className={`${toggleBackgroundClassname} ml-4 inline-flex h-6 w-11  items-center rounded-full`}
+      className={`${bgColor} ml-4 inline-flex h-6 w-11  items-center rounded-full`}
     >
       <span className="sr-only">Toggle dark mode</span>
-      {mode === null ? null : (
-        <span
-          className={`${
-            mode === 'dark'
-              ? ' translate-x-6 from-primary-200 to-primary-800'
-              : 'translate-x-1 from-primary-200 to-primary-800'
-          } inline-block h-4 w-4 transition-all duration-300 rounded-full bg-gradient-to-tr `}
-        />
-      )}
+      {mode === null ? null : <SunMoon mode={mode} />}
     </Switch>
   );
 }
